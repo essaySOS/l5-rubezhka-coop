7f3ce5f61146eeb8a7e85a3d662cf60dc1e47eff
essaySOS <corama.2000@mail.ru>
2024-11-12 15:57:03 +0300

    4

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+import PostalValidator from './src/PostalCodeSchema.js';
+import ipValidator from './src/IpAddressSchema.js';
 
+class Validator {
+  postalCode() {
+    return new PostalValidator();
+  }
+
+  ipAddress() {
+    return new ipValidator();
+  }
+}
+
+// const v = new Validator();
+// const postalCodeSchema = v.postalCode();
+// console.log(postalCodeSchema);
+
+// console.log(postalCodeSchema.isValid('ZIP_12345')); // true
+// postalCodeSchema.isValid('ZIP_67890'); // true
+// postalCodeSchema.isValid('ZIP_54321'); // true
+// postalCodeSchema.isValid('invalid-postal-code'); // false
+// console.log(postalCodeSchema.isValid(1234567890)); // false
+
+export default Validator;
---
+++ src/IpAddressSchema.js
@@ -0,0 +1,25 @@
+class ipValidator {
+    isRealIp(str) {
+        const digits = `${str}`.split('.');
+        return digits.at(0) === '27' && digits.length === 4;
+    }
+
+    isValid(str) {
+        return this.isRealIp(str);
+    }
+
+    ipAddressValues() {
+        this.valuesCheck = true;
+        return this;
+    }
+    isNormValues(str) {
+        if (!this.valuesCheck) return true;
+        const res = str.split('.').filter((digit) => digit >= 0 && digit < 256);
+        return res.length === 4;
+    }
+    isValid (str) {
+        return  this.isRealIp(str) && this.isNormValues(str);
+    }
+}
+
+export default ipValidator;
